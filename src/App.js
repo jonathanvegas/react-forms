7e1276e3f407e4b731f847f49a48e6a918b08df7
Jonathan Vegas <jonathanvegas@gmail.com>
2022-08-09 14:26:15 -0400

    new controls and format

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  const [title,setTitle] = useState("This is Title");
+  const [description, setDescription] = useState("your description");
+  const [author, setAuthor] = useState("todd albert");
+
+  console.log(title);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form>
+        <h1>Comments</h1>
+        {/* here goes the title */}
+        <label>Title</label>
+        <input 
+        type = "text" 
+        value={title}
+        onChange ={(e)=>{setTitle(e.target.value)}}
+        />
+        <h3>{title}</h3>
+        <label>Description</label>
+        <textarea 
+        value = {description}
+        onChange={(e)=>{setDescription(e.target.value)}}></textarea>
+        <h3>{description}</h3>
+
+        <label>Author</label>
+        <select value={author}
+        onChange={(e)=> {setAuthor(e.target.value)}}
         >
-          Learn React
-        </a>
-      </header>
+
+          <option value="" selected>Choose one</option>
+          <option value="Jonathan">Jonathan</option>
+          <option value="todd albert">Doctor Tod</option>
+          <option value="other">Other</option>
+        </select>
+        <h3>{author}</h3>
+
+        <button>Submit Form</button>
+
+      </form>
     </div>
   );
 }
